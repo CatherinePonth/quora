bb5680a402f8ebf7248bb1109511478de824bb75
Catherine Ponthieux <poncath@outlook.fr>
2022-07-17 22:44:16 +0200

    Affichage de la liste des questions

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,31 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $questions = [
+            [
+                'title' => 'Je suis une question',
+                'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, commodi praesentium! Non exercitationem accusamus natus, alias aut labore, quaerat eos atque ab debitis reprehenderit, corrupti similique fugit illo accusantium quasi!",
+                'rating' => -12,
+                'author' => [
+                    'name' => "Rachid Jeffali",
+                    'avatar' => "https://randomuser.me/api/portraits/lego/3.jpg"
+                ],
+                'nbResponse' => 15
+            ],
+            [
+                'title' => 'Je suis une question',
+                'content' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, commodi praesentium! Non exercitationem accusamus natus, alias aut labore, quaerat eos atque ab debitis reprehenderit, corrupti similique fugit illo accusantium quasi!",
+                'rating' => 15,
+                'author' => [
+                    'name' => "Julie Desflandres",
+                    'avatar' => "https://randomuser.me/api/portraits/women/24.jpg"
+                ],
+                'nbResponse' => 12
+            ]
+        ];
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'questions' => $questions,
         ]);
     }
 }
